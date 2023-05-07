f685f301b5ab5a5773bbdc779a1a7d174465a6f2
Ankit Mongia <ankit.mongia@algosec.com>
2023-05-07 10:38:19 +0530

    eventListeners: useState hook and mouse events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import Time from "./Time";
 
 function App() {
+
+  var [buttonColor, setButtonColor]= React.useState("white");
+
+  function handleMouseOut(){
+    setButtonColor("white");
+  }
+
+  function handleMouseOver(){
+    setButtonColor("black");
+  }
+
   return (
     <div className="container">
-      <Time />
+      <h1>Hello</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button style={{backgroundColor:buttonColor}} onMouseOut={handleMouseOut} onMouseOver={handleMouseOver}>Submit</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,33 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-import cars from "./practice";
-
-console.log(cars);
-const [honda, tesla]= cars;
-const {coloursByPopularity : [teslaTopColour]}= tesla;
-const {speedStats : {topSpeed :teslaTopSpeed}}= tesla;
-
-const {coloursByPopularity : [hondaTopColour]}= honda;
-const {speedStats : {topSpeed :hondaTopSpeed}}= honda;
-
-
-ReactDOM.render(
-  <table>
-    <tr>
-      <th>Brand</th>
-      <th>Top Speed</th>
-    </tr>
-    <tr>
-      <td>{tesla.model}</td>
-      <td>{teslaTopSpeed}</td>
-      <td>{teslaTopColour}</td>
-    </tr>
-    <tr>
-      <td>{honda.model}</td>
-      <td>{hondaTopSpeed}</td>
-      <td>{hondaTopColour}</td>
-    </tr>
-  </table>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
